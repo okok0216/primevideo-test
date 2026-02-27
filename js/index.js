b3d9d80632d3be4ec3401860b3e9613d20a6daa2
ej-lee77 <eunji0.0777@gmail.com>
2026-02-27 17:45:45 +0900

    플러그인-최근추가된콘텐츠

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 // 랭킹 swiper
-let swiper = new Swiper(".ranking-list-wrap", {
+let rankingSwiper = new Swiper(".ranking-list-wrap", {
     slidesPerView: 2,
     slidesPerGroupSkip: 1,
     // spaceBetween: 80,
     pagination: {
-        el: ".movie-pagination",
+        el: ".ranking-pagination",
     },
     navigation: {
-        nextEl: ".movie-button-next",
-        prevEl: ".movie-button-prev",
+        nextEl: ".ranking-button-next",
+        prevEl: ".ranking-button-prev",
     },
     breakpoints: {
         480: {
@@ -22,6 +22,36 @@ let swiper = new Swiper(".ranking-list-wrap", {
         },
         1280: {
             slidesPerView: 5
+        },
+    },
+});
+
+// 최근 추가된 콘텐츠, 계속 시청하기, 추천 영화, 추천 TV프로그램 swiper
+let movieSwiper = new Swiper(".new-contents-list-wrap", {
+    slidesPerView: 1.8,
+    slidesPerGroupSkip: 6,
+    pagination: {
+        el: ".new-contents-pagination",
+    },
+    navigation: {
+        nextEl: ".new-contents-button-next",
+        prevEl: ".new-contents-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1.8
+        },
+        768: {
+            slidesPerView: 3
+        },
+        980: {
+            slidesPerView: 4
+        },
+        1280: {
+            slidesPerView: 5
+        },
+        1300: {
+            slidesPerView: 6
         },
     },
 });
