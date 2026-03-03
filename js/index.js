1d7ee607d1b261eae611181805fb71de702bdbc9
ej-lee77 <eunji0.0777@gmail.com>
2026-03-03 16:10:39 +0900

    영상들 플러그인

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // 랭킹 swiper
 let rankingSwiper = new Swiper(".ranking-list-wrap", {
     slidesPerView: 2,
-    slidesPerGroupSkip: 1,
+    slidesPerGroup: 2,
     // spaceBetween: 80,
     pagination: {
         el: ".ranking-pagination",
@@ -12,24 +12,29 @@ let rankingSwiper = new Swiper(".ranking-list-wrap", {
     },
     breakpoints: {
         480: {
-            slidesPerView: 2
+            slidesPerView: 2,
+            slidesPerGroup: 2,
         },
         640: {
-            slidesPerView: 3
+            slidesPerView: 3,
+            slidesPerGroup: 3,
         },
         980: {
-            slidesPerView: 4
+            slidesPerView: 4,
+            slidesPerGroup: 4,
         },
         1280: {
-            slidesPerView: 5
+            slidesPerView: 5,
+            slidesPerGroup: 5,
         },
     },
 });
 
-// 최근 추가된 콘텐츠, 계속 시청하기, 추천 영화, 추천 TV프로그램 swiper
-let movieSwiper = new Swiper(".new-contents-list-wrap", {
+// 최근 추가된 콘텐츠 swiper
+let newcontentSwiper = new Swiper(".new-contents-list-wrap", {
     slidesPerView: 1.8,
-    slidesPerGroupSkip: 6,
+    slidesPerGroup: 1.8,
+    spaceBetween: 20,
     pagination: {
         el: ".new-contents-pagination",
     },
@@ -39,19 +44,132 @@ let movieSwiper = new Swiper(".new-contents-list-wrap", {
     },
     breakpoints: {
         640: {
-            slidesPerView: 1.8
+            slidesPerView: 1.8,
+            slidesPerGroup: 1.8,
         },
         768: {
-            slidesPerView: 3
+            slidesPerView: 3,
+            slidesPerGroup: 3,
         },
         980: {
-            slidesPerView: 4
+            slidesPerView: 4,
+            slidesPerGroup: 4,
         },
         1280: {
-            slidesPerView: 5
+            slidesPerView: 5,
+            slidesPerGroup: 5,
         },
         1300: {
-            slidesPerView: 6
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+        },
+    },
+});
+
+// 계속 시청하기 swiper
+let playingSwiper = new Swiper(".playing-list-wrap", {
+    slidesPerView: 1.8,
+    slidesPerGroup: 1.8,
+    spaceBetween: 20,
+    pagination: {
+        el: ".playing-pagination",
+    },
+    navigation: {
+        nextEl: ".playing-button-next",
+        prevEl: ".playing-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1.8,
+            slidesPerGroup: 1.8,
+        },
+        768: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+        },
+        980: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+        },
+        1280: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+        },
+        1300: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+        },
+    },
+});
+
+// 추천영화 swiper
+let recmovieSwiper = new Swiper(".rec-movie-list-wrap", {
+    slidesPerView: 1.8,
+    slidesPerGroup: 1.8,
+    spaceBetween: 20,
+    pagination: {
+        el: ".rec-movie-pagination",
+    },
+    navigation: {
+        nextEl: ".rec-movie-button-next",
+        prevEl: ".rec-movie-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1.8,
+            slidesPerGroup: 1.8,
+        },
+        768: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+        },
+        980: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+        },
+        1280: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+        },
+        1300: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+        },
+    },
+});
+
+// 추천TV프로그램 swiper
+let rectvSwiper = new Swiper(".rec-tv-list-wrap", {
+    slidesPerView: 1.8,
+    slidesPerGroup: 1.8,
+    spaceBetween: 20,
+    pagination: {
+        el: ".rec-tv-pagination",
+    },
+    navigation: {
+        nextEl: ".rec-tv-button-next",
+        prevEl: ".rec-tv-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1.8,
+            slidesPerGroup: 1.8,
+        },
+        768: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+        },
+        980: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+        },
+        1280: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+        },
+        1300: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
         },
     },
 });
