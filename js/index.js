9a8c50dcaffafd01aca4bd145aac480423eaa28e
ej-lee77 <eunji0.0777@gmail.com>
2026-03-05 16:56:55 +0900

    nav js 추가

--- js/index.js
+++ js/index.js
@@ -246,7 +246,7 @@ slider.addEventListener("transitionend", () => {
 
 window.addEventListener("resize", () => moveSlide(false));
 
-// footer lang선택
+// ----footer lang선택----------------------------------------
 // .lang-wrap click event 
 let langWrap = document.querySelector(".lang-wrap>a");
 let lang = document.querySelector(".lang");
@@ -272,3 +272,49 @@ langA.forEach((a)=>{
         langToggle();
     });
 });
+
+// ----공통함수----------------------------------------
+// 윈도우의 너비값 체크할 함수
+let wWidth;
+function siteInit() {
+    wWidth = window.innerWidth;
+}
+// 윈도우의 너비가 변경되면 윈도우의 너비값 다시 받기
+window.addEventListener("resize", () => {
+    siteInit();
+    subMenu.classList.remove("active");
+});
+
+// ----nav submenu----------------------------------------
+let header = document.querySelector("header");
+let mainMenuGenre = document.querySelector(".main-menu .mainmenu-genre");
+let subMenu = document.querySelector(".submenu-wrap");
+
+mainMenuGenre.addEventListener("mouseenter", ()=>{
+    if (wWidth > 640) {
+        subMenu.classList.add("active");
+    }
+});
+mainMenuGenre.addEventListener("mouseleave", () => {
+    if (wWidth > 640) {
+        subMenu.classList.remove("active");
+    }
+});
+
+// ----gnb usermenu----------------------------------------
+let profileMenu = document.querySelector(".profile-submenu");
+let gnbProfile = document.querySelector(".gnb-profile");
+
+gnbProfile.addEventListener("click", ()=>{
+    // 서브메뉴가 보이는지 체크하기
+    if (wWidth > 640) {
+        let isOpen = profileMenu.style.height && profileMenu.style.height !== "0px";
+        if (isOpen) {
+            profileMenu.style.height = 0;
+        } else {
+            let profileHeight = profileMenu.scrollHeight;
+            profileMenu.style.height = profileHeight + "px";
+        }
+    }
+});
+
