d8300bdbab289ad3744d609160f322298073b894
wldbs2334 <wldbs2334@gmail.com>
2026-03-11 17:47:52 +0900

    수정중3

--- js/sports.js
+++ js/sports.js
@@ -44,7 +44,7 @@ let liveSwiper = new Swiper(".sports-live-wrap", {
 let highLightSwiper = new Swiper(".highlight-list-wrap", {
   slidesPerView: 6,
   slidesPerGroup: 4,
-  spaceBetween: 15,
+  spaceBetween: 20,
   pagination: {
     el: ".highlight-pagination"
   },
@@ -58,12 +58,40 @@ let highLightSwiper = new Swiper(".highlight-list-wrap", {
 let recMovieSwiper = new Swiper(".rec-movie-wrap", {
   slidesPerView: 6,
   slidesPerGroup: 4,
-  spaceBetween: 15,
+  spaceBetween: 20,
   pagination: {
     el: ".rec-movie-pagination"
   },
   navigation: {
     nextEl: ".rec-movie-button-next",
     prevEl: ".rec-movie-button-prev"
+  }
+})
+
+//오리지널
+let originalSwiper = new Swiper(".original-wrap", {
+  slidesPerView: 5,
+  slidesPerGroup: 6,
+  spaceBetween: 30,
+  pagination: {
+    el: ".original-pagination"
+  },
+  navigation: {
+    nextEl: ".original-button-next",
+    prevEl: ".original-button-prev"
+  }
+})
+
+//추천시리즈
+let recTvSwiper = new Swiper(".rec-series-wrap", {
+  slidesPerView: 6,
+  slidesPerGroup: 4,
+  spaceBetween: 20,
+  pagination: {
+    el: ".rec-series-pagination"
+  },
+  navigation: {
+    nextEl: ".rec-series-button-next",
+    prevEl: ".rec-series-button-prev"
   }
 })
